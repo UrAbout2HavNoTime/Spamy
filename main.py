2250a9ee1ab3f88584abb5853649345941277ea6
Ur_About2HavNoTime <98365267+UrAbout2HavNoTime@users.noreply.github.com>
2022-03-19 10:38:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import keyboard,time
+import keyboard,time,pyautogui
 spam = input('What do you want to spam to your friends >? ')
 count = input('How much times before pressing enter>?')
 print('Scipt ready and loaded now get on the chat in under 3 seconds')
@@ -8,9 +8,10 @@ counter = 0
 while True:
     for letter in spam:
         try:
-            keyboard.press(letter)
+            pyautogui.press(letter)
         except TypeError:
             print('dang it no work')
+
         time.sleep(.015)
     time.sleep(0.015)
     keyboard.press('shift')
